cbeb02c53d79ef713baf4ccf6ae048faded469f3
georgedevops <george97dj@gmail.com>
2024-04-01 16:10:07 -0400

    updated

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../navigation/navigation.component';
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive, RouterOutlet],
+  imports: [RouterLink,RouterLinkActive, RouterOutlet,NavigationComponent],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css'
 })
